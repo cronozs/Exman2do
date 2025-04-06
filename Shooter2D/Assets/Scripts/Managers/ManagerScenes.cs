0905db0197e5603251823043d13b8d5c3e8e7ecd
Carlos Ramos <crono_zs@hotmail.com>
2025-04-06 11:03:55 -0600

    agregando menus y sprites de monedas

---
+++ Shooter2D/Assets/Scripts/Managers/ManagerScenes.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Shooter
+{
+    public class ManagerScenes : MonoBehaviour
+    {
+        public void SceneChanger(int scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
